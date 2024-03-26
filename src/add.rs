9775d29c18bbdb1d65318ba2d6de4e37a2ac3748
Colt <austincgause@gmail.com>
2024-03-25 22:17:06 -0400

    added base implementation

---
+++ src/add.rs
@@ -0,0 +1,3 @@
+pub fn add_item(_tokens: Vec<String>) {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+mod add;
+
+// use sqlx::sqlite::SqlitePoolOptions;
+// use std::env;
+use clap::{ Args, Parser, Subcommand };
+use add::add_item;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+
+    #[command(subcommand)]
+    command: Commands,
+
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Add a task
+    Add(AddArgs),
+    /// Delete a task
+    Delete,
+    /// Mark a task as complete
+    Complete,
+}
+
+#[derive(Args)]
+struct AddArgs {
+    task: Vec<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Add(add_task_params) => add_item(add_task_params.task),
+        Commands::Delete => todo!(),
+        Commands::Complete => todo!(),
+    }
+
 }
